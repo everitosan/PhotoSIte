02ca336d7a58207b80492ade44e6133df68227c3
Everardo Sánchez Hernández <everardo.ipn@gmail.com>
2018-04-14 01:14:32 -0500

    refact: add background detail to modal component

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -6,6 +6,20 @@ function Modal (props) {
   return (
     <div className={"ModalContainer " + (props.hide? 'hide': 'show') }>
       <div className="ModalBody" >
+        <div className="background" >
+          <div className="bg1 bg" ></div>
+          <div className="bg2 bg" ></div>
+
+          <div className="bg3 bg" ></div>
+          <div className="bg4 bg" ></div>
+
+          <div className="bg5 bg" ></div>
+          <div className="bg6 bg" ></div>
+
+          <div className="bg7 bg" ></div>
+          <div className="bg8 bg" ></div>
+
+        </div>
         <Link to="/"> <div className="closeModal">  </div> </Link>
         <div className="modalContent">
           {props.children}
